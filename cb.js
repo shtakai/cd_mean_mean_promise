ce91c49d727e9c417931351b0ef20295c3571b34
Alyson Takai <shtakai@gmail.com>
2016-07-16 21:19:07 -0700

    donut callback

---
+++ cb.js
@@ -0,0 +1,33 @@
+// failed example
+function buyDonut(){
+  orderDonut();
+  console.log('buyDonut:pay');
+  console.log('buyDonut:walk out door');
+}
+
+// function that gets a donut
+function orderDonut(){
+  let x = setTimeout( () => {
+    console.log('orderDonut:donut on counter')
+  }, 1000);
+}
+
+buyDonut();
+console.log('-----');
+// callback example
+function buyDonut2(){
+  orderDonut2( () => {
+    console.log('buyDonut2:pay');
+    console.log('buyDonut2:walk out door');
+  });
+}
+
+// function that gets a donut
+function orderDonut2(callback){
+  let x = setTimeout( () => {
+    console.log('orderDonut2:donut is on counter');
+    callback();
+  }, 1000 );
+}
+
+buyDonut2();
